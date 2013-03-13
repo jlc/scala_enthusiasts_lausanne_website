e82768f3ecd81690da7fb80dbe72c9dca50c71ce
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2013-03-13 15:08:03 +0100

    initial application with menu and internationalisation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,36 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.i18n.Lang
+import play.api.Play.current
+
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def authenticated(f: (String) => Result)(implicit request: Request[_]): Option[Result] = {
+    request.session.get("well_known_user").map { user => f(user) }
+  }
+      
+  def index() = Action { implicit request =>
+    //println("Languages: " + request.acceptLanguages.map(_.code).mkString(","))
+    val lang = Lang.get("en") getOrElse Lang.preferred(request.acceptLanguages)
+
+    authenticated { user =>      
+      Ok(views.html.index()(lang))
+    } getOrElse {
+      Ok(views.html.index()(lang))
+    }
+  }
+
+  def meetings = Action {
+    Ok(views.html.meetings())
+  }
+
+  def address = Action {
+    Ok(views.html.address())
+  }
+
+  def sponsors = Action {
+    Ok(views.html.sponsors())
   }
   
 }
