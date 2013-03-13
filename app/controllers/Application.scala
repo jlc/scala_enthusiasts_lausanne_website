ef9ee584eb518275a1b205b019a1378f3953af34
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2013-03-13 16:03:51 +0100

    implement language selection

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,36 +3,52 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.Logger
 import play.api.i18n.Lang
 import play.api.Play.current
 
 object Application extends Controller {
   
-  def authenticated(f: (String) => Result)(implicit request: Request[_]): Option[Result] = {
-    request.session.get("well_known_user").map { user => f(user) }
+  private object SessionValue {
+    val Lang = "lang"
+    val User = "well_known_user"
   }
-      
-  def index() = Action { implicit request =>
-    //println("Languages: " + request.acceptLanguages.map(_.code).mkString(","))
-    val lang = Lang.get("en") getOrElse Lang.preferred(request.acceptLanguages)
 
-    authenticated { user =>      
-      Ok(views.html.index()(lang))
+  private def authenticated(f: (String) => Result)(implicit request: Request[_]): Option[Result] = {
+    request.session.get(SessionValue.User).map { user => f(user) }
+  }
+
+  private def clientLanguage(implicit request: Request[_]) =
+    request.session.get(SessionValue.Lang) map { Lang(_) } getOrElse { Lang.preferred(request.acceptLanguages) }
+
+
+
+  def index() = Action { implicit request =>
+    Ok(views.html.index()(clientLanguage))
+  }
+
+  def meetings = Action { implicit request =>
+    Ok(views.html.meetings()(clientLanguage))
+  }
+
+  def address = Action { implicit request =>
+    Ok(views.html.address()(clientLanguage))
+  }
+
+  def sponsors = Action { implicit request =>
+    Ok(views.html.sponsors()(clientLanguage))
+  }
+  
+  def lang(l: String) = Action { implicit request =>
+    def redirect = Redirect(routes.Application.index())
+
+    Lang.get(l) map { lang =>
+      Logger.debug("lang: Setting lang to: " + l)
+      redirect.withSession(SessionValue.Lang -> l)
     } getOrElse {
-      Ok(views.html.index()(lang))
+      Logger.debug("lang: Removing previous setting of lang")
+      redirect.withSession(session - SessionValue.Lang)
     }
   }
 
-  def meetings = Action {
-    Ok(views.html.meetings())
-  }
-
-  def address = Action {
-    Ok(views.html.address())
-  }
-
-  def sponsors = Action {
-    Ok(views.html.sponsors())
-  }
-  
 }
