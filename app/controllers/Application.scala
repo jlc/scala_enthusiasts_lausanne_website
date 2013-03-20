45cc2fea0a6429589c42a8eb6488b7f95067a6c9
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2013-03-20 15:42:23 +0100

    initial authentication implementation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,13 +7,23 @@ import play.api.Logger
 import play.api.i18n.Lang
 import play.api.Play.current
 
+import play.api.data._
+import play.api.data.Forms._
+
+import play.api.libs.json.Json
+
+/*
 import com.shorrockin.cascal.utils.Conversions._
 import com.shorrockin.cascal.session.{Host, PoolParams, SessionPool, ExhaustionPolicy, Consistency}
+ */
+
+import models.User
 
 object Application extends Controller with ControllerHelper {
 
   def index() = Action { implicit request =>
 
+    /*
     val hosts  = Host("localhost", 9160, 250) :: Nil
     val params = new PoolParams(10, ExhaustionPolicy.Fail, 500L, 6, 2)
     val pool   = new SessionPool(hosts, params, Consistency.One)
@@ -24,6 +34,7 @@ object Application extends Controller with ControllerHelper {
       val value = session.get("segl" \ "Users" \ "key" \ "realname")
       println("XXX after inserting, here is the value: " + value)
     }
+     */
 
     Ok(views.html.index()(User.anonymous, clientLanguage))
   }
@@ -52,4 +63,32 @@ object Application extends Controller with ControllerHelper {
     }
   }
 
+  def admin = Action { implicit request =>
+    Ok("")
+  }
+
+  // Remote procedure
+
+  def authenticate = Action { implicit request =>
+
+    val loginForm = Form(
+      tuple(
+        "email" -> email,
+        "password" -> nonEmptyText
+      )
+    )
+
+    val json =
+      loginForm.bindFromRequest.fold(
+        loginWithErrors => {
+          Json.obj("return" -> false, "error" -> "Invalid authentication")
+        },
+        login => {
+          Logger.debug("authenticate: " + login)
+          Json.obj("return" -> true)
+        }
+      )
+
+    Ok(Json.stringify(json))
+  }
 }
