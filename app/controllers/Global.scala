35648e2a96de8c22aff6ca3190d87196931a4e32
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2013-03-21 11:36:49 +0100

    add global object (still useless for now)

---
+++ app/controllers/Global.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.{GlobalSettings, Application}
+
+object Global extends GlobalSettings {
+
+  override def onStart(app: Application) {
+  }
+
+  override def onStop(app: Application) {
+  }
+
+}
