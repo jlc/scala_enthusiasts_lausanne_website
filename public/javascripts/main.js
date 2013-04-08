ebe84d873fa76244f32736225279cf864005dda5
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2013-04-08 18:26:24 +0200

    add new files in the middle of work

---
+++ public/javascripts/main.js
@@ -0,0 +1,68 @@
+/*
+ * main.js
+ */
+
+
+angular.module('segl', ['common', 'ui']).
+
+    // configure view routes
+    config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	    // index
+	    when('/index', {
+		templateUrl: '/templates/index',
+		controller: IndexCtrl
+	    }).
+
+	    // meetings
+	    when('/meetings/agenda', {
+		templateUrl: '/templates/meetings/agenda',
+		controller: AgendaCtrl
+	    }).
+	    when('/meetings/sessions', {
+		templateUrl: '/templates/meetings/sessions',
+		controller: SessionsCtrl
+	    }).
+	    when('/meetings/speakers', {
+		templateUrl: '/templates/meetings/speakers',
+		controller: SpeakersCtrl
+	    }).
+
+	    // address
+	    when('/address/map', {
+		templateUrl: '/templates/address/map',
+		controller: MapCtrl
+	    }).
+	    when('/address/contact', {
+		templateUrl: '/templates/address/contact',
+		controller: ContactCtrl
+	    }).
+
+	    // sponsors
+	    when('/sponsors/crossing-tech', {
+		templateUrl: '/templates/sponsors/crossing-tech',
+		controller: XtechCtrl
+	    }).
+	    when('/sponsors/epfl', {
+		templateUrl: '/templates/sponsors/epfl',
+		controller: EpflCtrl
+	    }).
+
+	    // admin
+	    when('/admin/edit-introduction', {
+		templateUrl: '/templates/admin/edit-introduction',
+		controller: EditIntroductionCtrl
+	    }).
+	    when('/admin/edit-announcement', {
+		templateUrl: '/templates/admin/edit-announcement',
+		controller: EditAnnouncementCtrl
+	    }).
+	    when('/admin/edit-sessions', {
+		templateUrl: '/templates/admin/edit-sessions',
+		controller: EditSessionsCtrl
+	    }).
+
+	    otherwise({redirectTo: '/index'});
+    }]);
+
+
